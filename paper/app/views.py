6f76640ad3b4f6355feed8b97fcb12d230f6fdf9
manhcuongdoan <doanmanhcuong1234567@gmail.com>
2019-11-28 10:40:22 +0700

    setting framework

---
+++ paper/app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+@api_view(['GET'])
+def news(request):
+    return Response(
+        {'message': 'news'}
+    )
+
